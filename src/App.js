3c542f4712e98b73e662795e58493a91ff8a334c
muhammed asiyas <muhammedasiyas18@gmail.com>
2025-07-29 21:41:22 +0530

    Solution

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {BrowserRouter, Route, Switch} from 'react-router-dom'
+import {Route, Switch} from 'react-router-dom'
 import Home from './components/Home/home'
 import Header from './components/Header/header'
 import Projects from './components/Projects/project'
@@ -7,16 +7,16 @@ import About from './components/About/about'
 import NotFound from './components/NotFound/notfound'
 
 const App = () => (
-    <BrowserRouter>
+    <>
       <Header />
-        <Switch>
-          <Route exact path='/' component={Home} />
-          <Route exact path="/project" component={Projects} />
-          <Route exact path="/contact" component={Contact}/>
-          <Route exact path="/about" component={About}/>
-          <Route component={NotFound} />
-        </Switch>
-    </BrowserRouter>
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route exact path="/project" component={Projects} />
+        <Route exact path="/contact" component={Contact}/>
+        <Route exact path="/about" component={About}/>
+        <Route component={NotFound} />
+      </Switch>
+    </>
 )
 
 export default App
